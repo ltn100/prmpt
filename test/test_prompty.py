5feca46d48717a9cb4e80c877d1c3d046fd91dd9
ltn100 <ltn100@gmail.com>
2014-08-09 19:50:26 +0100

    More support for 2.6
    Removed unicode identifier for strings for Pytohn 3.0
    
    Added io as a fallback for StringIO or Python 3

--- test/test_prompty.py
+++ test/test_prompty.py
@@ -13,7 +13,11 @@ import tempfile
 import unittest
 import distutils.spawn
 from contextlib import contextmanager
-from StringIO import StringIO
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from io import StringIO
+
 
 TEST_DIR = os.path.dirname(__file__)
 
@@ -24,6 +28,16 @@ import prompty
 
 prompty_bin = imp.load_source("prompty_bin", os.path.join(os.path.dirname(__file__), "..", "bin", "prompty"))
 
+
+_MAX_LENGTH = 80
+def safe_repr(obj, short=False):
+    try:
+        result = repr(obj)
+    except Exception:
+        result = object.__repr__(obj)
+    if not short or len(result) < _MAX_LENGTH:
+        return result
+    return result[:_MAX_LENGTH] + ' [truncated]...'
 
 class UnitTestWrapper(unittest.TestCase):
     """
@@ -57,6 +71,108 @@ if True or "assertIsInstance" not in dir(UnitTestWrapper):
             standardMsg = '%s is not an instance of %r' % (safe_repr(obj), cls)
             self.fail(self._formatMessage(msg, standardMsg))
     setattr(UnitTestWrapper, "assertIsInstance", _assertIsInstance)
+
+if True or "assertSequenceEqual" not in dir(UnitTestWrapper):
+    def _assertSequenceEqual(self, seq1, seq2, msg=None, seq_type=None):
+        """An equality assertion for ordered sequences (like lists and tuples).
+
+        For the purposes of this function, a valid ordered sequence type is one
+        which can be indexed, has a length, and has an equality operator.
+
+        Args:
+            seq1: The first sequence to compare.
+            seq2: The second sequence to compare.
+            seq_type: The expected datatype of the sequences, or None if no
+                    datatype should be enforced.
+            msg: Optional message to use on failure instead of a list of
+                    differences.
+        """
+        if seq_type is not None:
+            seq_type_name = seq_type.__name__
+            if not isinstance(seq1, seq_type):
+                raise self.failureException('First sequence is not a %s: %s'
+                                        % (seq_type_name, safe_repr(seq1)))
+            if not isinstance(seq2, seq_type):
+                raise self.failureException('Second sequence is not a %s: %s'
+                                        % (seq_type_name, safe_repr(seq2)))
+        else:
+            seq_type_name = "sequence"
+
+        differing = None
+        try:
+            len1 = len(seq1)
+        except (TypeError, NotImplementedError):
+            differing = 'First %s has no length.    Non-sequence?' % (
+                    seq_type_name)
+
+        if differing is None:
+            try:
+                len2 = len(seq2)
+            except (TypeError, NotImplementedError):
+                differing = 'Second %s has no length.    Non-sequence?' % (
+                        seq_type_name)
+
+        if differing is None:
+            if seq1 == seq2:
+                return
+
+            seq1_repr = safe_repr(seq1)
+            seq2_repr = safe_repr(seq2)
+            if len(seq1_repr) > 30:
+                seq1_repr = seq1_repr[:30] + '...'
+            if len(seq2_repr) > 30:
+                seq2_repr = seq2_repr[:30] + '...'
+            elements = (seq_type_name.capitalize(), seq1_repr, seq2_repr)
+            differing = '%ss differ: %s != %s\n' % elements
+
+            for i in xrange(min(len1, len2)):
+                try:
+                    item1 = seq1[i]
+                except (TypeError, IndexError, NotImplementedError):
+                    differing += ('\nUnable to index element %d of first %s\n' %
+                                 (i, seq_type_name))
+                    break
+
+                try:
+                    item2 = seq2[i]
+                except (TypeError, IndexError, NotImplementedError):
+                    differing += ('\nUnable to index element %d of second %s\n' %
+                                 (i, seq_type_name))
+                    break
+
+                if item1 != item2:
+                    differing += ('\nFirst differing element %d:\n%s\n%s\n' %
+                                 (i, item1, item2))
+                    break
+            else:
+                if (len1 == len2 and seq_type is None and
+                    type(seq1) != type(seq2)):
+                    # The sequences are the same, but have differing types.
+                    return
+
+            if len1 > len2:
+                differing += ('\nFirst %s contains %d additional '
+                             'elements.\n' % (seq_type_name, len1 - len2))
+                try:
+                    differing += ('First extra element %d:\n%s\n' %
+                                  (len2, seq1[len2]))
+                except (TypeError, IndexError, NotImplementedError):
+                    differing += ('Unable to index element %d '
+                                  'of first %s\n' % (len2, seq_type_name))
+            elif len1 < len2:
+                differing += ('\nSecond %s contains %d additional '
+                             'elements.\n' % (seq_type_name, len2 - len1))
+                try:
+                    differing += ('First extra element %d:\n%s\n' %
+                                  (len1, seq2[len1]))
+                except (TypeError, IndexError, NotImplementedError):
+                    differing += ('Unable to index element %d '
+                                  'of second %s\n' % (len1, seq_type_name))
+        standardMsg = differing
+        msg = self._formatMessage(msg, standardMsg)
+        self.fail(msg)
+    setattr(UnitTestWrapper, "assertSequenceEqual", _assertSequenceEqual)
+
 
 @contextmanager
 def captured_output():
@@ -614,22 +730,22 @@ class StandardFunctionTests(UnitTestWrapper):
 
     def test_dollar(self):
         c = prompty.functionContainer.FunctionContainer()
-        self.assertEqual(ur"$", c._call(["dollar"]))
-        self.assertEqual(ur"#", c._call(["dollar"],0))
+        self.assertEqual(r"$", c._call(["dollar"]))
+        self.assertEqual(r"#", c._call(["dollar"],0))
 
     def test_specialChars(self):
         c = prompty.functionContainer.FunctionContainer()
         chars = [
-                 ("newline",            u"\n"),
-                 ("carriagereturn",     u"\r"),
-                 ("space",              u" "),
-                 ("backslash",          u"\\"),
-                 ("percent",            u"%"),
-                 ("opencurly",          u"{"),
-                 ("closecurly",         u"}"),
-                 ("opensquare",         u"["),
-                 ("closesquare",        u"]"),
-                 ("escape",             u"\033")
+                 ("newline",            "\n"),
+                 ("carriagereturn",     "\r"),
+                 ("space",              " "),
+                 ("backslash",          "\\"),
+                 ("percent",            "%"),
+                 ("opencurly",          "{"),
+                 ("closecurly",         "}"),
+                 ("opensquare",         "["),
+                 ("closesquare",        "]"),
+                 ("escape",             "\033")
                 ]
         for char in chars:
             self.assertEqual(char[1], c._call([char[0]]))
