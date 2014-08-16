8949807ecc2df1c3a289475f7c9413a0af813369
ltn100 <ltn100@gmail.com>
2014-08-16 17:35:13 +0100

    Added post-build step to compile promptyc file before install

--- setup.py
+++ setup.py
@@ -2,24 +2,47 @@
 # vim:set softtabstop=4 shiftwidth=4 tabstop=4 expandtab:
 
 from distutils.core import setup
+from distutils.command.install_data import install_data
+from distutils.command.build_py import build_py
+import py_compile
 import glob
 import os
 
+
+class PostBuild(build_py):
+    def run(self):
+        build_py.run(self)
+        print "compiling promptyc"
+        py_compile.compile('bin/prompty')
+
+
 setup(name='prompty',
-      version='0.1',
-      description='A command line prompt markup language',
-      author='Lee Netherton',
-      author_email='lee.netherton@gmail.com',
-      url='https://github.com/ltn100/prompty',
-      license='MIT licence, see LICENCE',
-      packages=['prompty'],
-      scripts=['bin/prompty','bin/promptyc'],
-      data_files=[
+    # Must comply with http://legacy.python.org/dev/peps/pep-0440/#version-scheme
+    version='0.1rc1',
+
+    description='A command line prompt markup language',
+
+    author='Lee Netherton',
+    author_email='lee.netherton@gmail.com',
+    url='https://github.com/ltn100/prompty',
+    license='MIT licence, see LICENCE',
+
+    packages=['prompty'],
+
+    scripts=['bin/prompty','bin/promptyc'],
+
+    data_files=[
         ('share/prompty/skel',
-         [f for f in glob.glob("skel/*") if os.path.isfile(f)]
-         ),
+            [f for f in glob.glob("skel/*") if os.path.isfile(f)]
+        ),
         ('share/prompty/skel/functions',
-         [f for f in glob.glob("skel/functions/*") if os.path.isfile(f)]
-         ),
-      ]
-     )
+            [f for f in glob.glob("skel/functions/*") if os.path.isfile(f)]
+        ),
+    ],
+
+    cmdclass={
+        # Post build step
+        'build_py': PostBuild,
+    },
+)
+
