6c6215372590f4002dbe5d7b1d005b2cb273bfd4
ltn100 <ltn100@gmail.com>
2015-12-24 23:05:06 +0000

    Improved coverage

--- test/test_vcs.py
+++ test/test_vcs.py
@@ -129,11 +129,100 @@ class SvnTests(UnitTestWrapper):
         self.assertIsInstance(g, prompty.vcs.VCSBase)
         self.assertEquals(g.command, prompty.svn.SVN_COMMAND)
 
-    def test_commandAvailable(self):
-        svn_installed = bool(distutils.spawn.find_executable(prompty.svn.SVN_COMMAND))
-        s = prompty.svn.Subversion(prompty.status.Status(0))
-        self.assertEquals(svn_installed, s.installed)
-        s = prompty.svn.Subversion(prompty.status.Status(0), "bogus_command_foo")
-        self.assertEquals(False, s.installed)
+    @mock.patch('prompty.vcs.subprocess')
+    def test_svnUnavailable(self, mock_sp):
+        # Set up mock
+        output = (
+            "",
+            "svn: command not found",
+            127,
+            OSError
+        )
+        mock_sp.Popen.return_value = MockProc(output)
 
+        g = prompty.svn.Subversion(prompty.status.Status(0))
+        self.assertEquals(False, g.installed)
+        self.assertEquals(False, g.isRepo)
+        self.assertEquals("", g.branch)
+        self.assertEquals(0, g.changed)
+        self.assertEquals(0, g.untracked)
 
+    @mock.patch('prompty.vcs.subprocess')
+    def test_notARepo(self, mock_sp):
+        # Set up mock
+        output = (
+            "",
+            "svn: warning: W155007: '/home/user/myrepo' is not a working copy\n",
+            0,
+            None
+        )
+        mock_sp.Popen.return_value = MockProc(output)
+
+        g = prompty.svn.Subversion(prompty.status.Status(0))
+        self.assertEquals(True, g.installed)
+        self.assertEquals(False, g.isRepo)
+        self.assertEquals("", g.branch)
+        self.assertEquals(0, g.changed)
+        self.assertEquals(0, g.staged)
+        self.assertEquals(0, g.unmerged)
+        self.assertEquals(0, g.untracked)
+
+    @mock.patch('prompty.vcs.subprocess')
+    def test_info(self, mock_sp):
+        # Set up mock
+        output = (
+            '<?xml version="1.0" encoding="UTF-8"?>\n' +
+            '<info>\n' +
+            '<entry\n' +
+            '   path="."\n' +
+            '   revision="5228"\n' +
+            '   kind="dir">\n' +
+            '<url>https://localhost/repos/myrepo/trunk</url>\n' +
+            '<relative-url>^/trunk</relative-url>\n' +
+            '<repository>\n'+
+            '<root>https://localhost/repos/myrepo</root>\n' +
+            '<uuid>1cf8179b-9667-814b-b662-e110218c33db</uuid>\n' +
+            '</repository>\n' +
+            '<wc-info>\n' +
+            '<wcroot-abspath>/home/user/myrepo</wcroot-abspath>\n' +
+            '<schedule>normal</schedule>\n' +
+            '<depth>infinity</depth>\n' +
+            '</wc-info>\n' +
+            '<commit\n' +
+            '   revision="5228">\n' +
+            '<author>ltn100</author>\n' +
+            '<date>2015-05-01T21:29:01.683640Z</date>\n' +
+            '</commit>\n' +
+            '</entry>\n' +
+            '</info>\n',
+            "",
+            0,
+            None
+        )
+        mock_sp.Popen.return_value = MockProc(output)
+
+        g = prompty.svn.Subversion(prompty.status.Status(0))
+        self.assertEquals(True, g.installed)
+        self.assertEquals(True, g.isRepo)
+        self.assertEquals("trunk", g.branch)
+
+    @mock.patch('prompty.vcs.subprocess')
+    def test_status(self, mock_sp):
+        # Set up mock
+        output = (
+            'M       test/myfile1.txt\n' +
+            'A       test/myfile2.txt\n' +
+            'C       test/myfile3.txt\n' +
+            'D       test/myfile4.txt\n' +
+            '!       test/myfile5.txt\n' +
+            '?       test/myfile6.txt\n' +
+            '?       test/myfile7.txt\n',
+            "",
+            0,
+            None
+        )
+        mock_sp.Popen.return_value = MockProc(output)
+
+        g = prompty.svn.Subversion(prompty.status.Status(0))
+        self.assertEquals(5, g.changed)
+        self.assertEquals(2, g.untracked)
